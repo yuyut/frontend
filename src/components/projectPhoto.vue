18dd62ef89de8e15424c60053d7c4afb8d801ae8
yu tung <qweroo0528@gmail.com>
2020-11-25 22:24:53 +0800

    change a lot

--- src/components/projectPhoto.vue
+++ src/components/projectPhoto.vue
@@ -2,30 +2,80 @@
   <div>
     <v-card>
       <v-card-title  :style="{height:'60px'}">
-        <v-btn 
-          color="primary"
-          dark
-          id="uploadBtn"
-          class:="px-3"
-        >
-        篩選
-        </v-btn>
+        Photos
+        <v-dialog
+          v-model="dialog"
+          persistent
+          max-width="600px"
+          >
+        <template v-slot:activator="{ on }">
+            <v-btn 
+                color="primary"
+                dark
+                v-on="on"
+                id="uploadBtn"
+                class:="px-3"
+            >
+                新增圖片
+          </v-btn>
+        </template>
+        <v-spacer></v-spacer>  
+          <v-card>
+            <v-card-title>
+                <span class="headline">{{$t('uploadTemplate')}}</span>
+            </v-card-title>
+            <v-card-text>
+              <v-file-input 
+              v-model="file"
+              :rules="rules"
+              id="docfile"
+              accept="image/*"
+              label="File input (image)"
+              ></v-file-input>
+          </v-card-text>
+          <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+              color="blue darken-1"
+              text
+              @click="dialog = false;"
+              >
+              {{$t('close')}}
+              </v-btn>
+              <v-btn
+              :disabled="buttonDisable()"
+              color="blue darken-1"
+              text
+              @click="dialog = false; myUpload();"
+              >
+              {{$t('save')}}
+              </v-btn>
+          </v-card-actions>
+          </v-card>
+      </v-dialog>    
       </v-card-title>
       <v-spacer></v-spacer>
-      <div v-if="is_data_fetched"> 
-        <v-row >
+      <div v-if="is_data_fetched" > 
+        <v-row :align="'end'" >
           <v-col
-            cols="12" sm="3" md="4"
-            v-for="imageURL in imageURLs"
-            :key="imageURL"
-            class="d-flex child-flex"
+            cols="12"  lg="2" sm="3" md="4" xs="6"
+            v-for="(photo, index) in photosData"
+            :key="index"
+          > 
+          <v-hover
+            v-slot="{ hover }"
+            open-delay="200"
           >
+          <v-card
+          :elevation="hover ? 12 : 2">
             <v-img
-              :src="imageURL"
-              
-              aspect-ratio="1"
+              max-height="350"
+              contain
+              :src="photo.image"
+              aspect-ratio="1.6"
               class="grey lighten-2"
             >
+            <div class="bottom-gradient"></div>
               <template v-slot:placeholder>
                 <v-row
                   class="fill-height ma-0"
@@ -38,12 +88,14 @@
                   ></v-progress-circular>
                 </v-row>
               </template>
-            </v-img>
-          </v-col>
-        </v-row>
-        </div>
-       
-       <!-- <v-dialog 
+            </v-img> 
+            <div class="text-bottom"> 
+              MONKEY MONKEY MONKEY</div> 
+              <div class="text-bottom"> 
+                建立者: {{photo.createdUserName}}    建立時間: {{time(photo.createdDate)}}  
+              <UserMenu :userInfo="{id:12,name:'asdfasdfsdf' }" />
+              </div> 
+               <!-- <v-dialog 
           v-model="dialog"
           persistent
           max-width="1200px"
@@ -125,13 +177,18 @@
             </v-card-text>
         </v-card>
       </v-dialog>    -->
+          </v-card>
+          </v-hover>
+          </v-col>
+        </v-row>
+        </div>
     </v-card>
   </div>
 </template>
 
 <script>
-
-
+import moment from 'moment'
+import UserMenu from './SystemUserMenu'
 export default {
 
   components: {
@@ -140,14 +197,31 @@ export default {
   props: {
 
   },
+  watch:{
+    photosData: {
+      deep: true,
+      
+      // We have to move our method to a handler field
+      handler(){
+        console.log('The list of colours has changed!');
+        this.$forceUpdate();
+      }
+      
+    }
+
+  },
   data() {
     return {
+      componentKey: 0,
       currentImage:null,
       dialog:false,
       imageURLs:[],
       photosData:[],
       is_data_fetched: false,
-
+      file:null,
+      rules: [
+        v => !!v || 'Required Content',
+      ],
     };
   },
   computed: {
@@ -156,6 +230,34 @@ export default {
     },
   },
   methods: {
+    time(date){
+        let time = moment(date).format('YYYY/MM/DD hh:mm:ss');
+        return time;
+    },
+    forceRerender() {
+      this.componentKey += 1;
+      console.log("forced");
+    },
+     myUpload:function(){
+        var file = this.file;    //name = file
+        var formdata = new FormData();
+        formdata.append('file',file);
+        let vm = this;
+        vm.$API.api.main.projectPhoto.post(this.projectId,'GeneralPicture',formdata)            
+        .then(res => {
+            console.log("uploaded");
+            vm.getPhotoId();       
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    },
+    buttonDisable(){
+      if(this.file)
+          return false;
+      else 
+          return true;
+    },
     checkCurrentImage(image){
       console.log(image);
       //this.currentImage = image;
@@ -171,12 +273,10 @@ export default {
         .then(res => {
           const reader = new FileReader();
           if (res.data) {
-
-            vm.imageURLs[i] = reader.readAsDataURL(res.data);
-            reader.onload = function(e) {
-            vm.imageURLs[i]=e.target.result;
-            
-            
+            vm.photosData[i]["image"] = reader.readAsDataURL(res.data);
+            reader.onloadend = function() { 
+            vm.photosData[i]["image"] = reader.result;       
+             //vm.$forceUpdate();      
             };
           }
         })
@@ -186,21 +286,23 @@ export default {
       }
       vm.is_data_fetched = true;
       console.log(this.imageURLs);
+     
     },
 
-    getPhotoId() {
+    async getPhotoId() {
       let vm = this;
-      this.$API.api.main.projectPhoto.get(this.projectId)            
+      await this.$API.api.main.projectPhoto.get(this.projectId)            
       .then(res => {
           if (res.data) {
             vm.photosData = res.data;
-            debugger
             vm.getPhotos();
           }
+           
       })
       .catch(function (error) {
           console.log(error);
       }); 
+       
     },
     
   },
@@ -212,13 +314,17 @@ export default {
   },
   beforeDestroy() {
     
-  },
-  watch: {
   }
-
 }
 </script>
 
 <style >
-
+  .bottom-gradient {
+    background-image: linear-gradient(to top, rgba(0, 0, 0, 0.4) 0%, transparent 72px);
+    vertical-align: bottom;
+    height: 100%;
+  }
+  .text-bottom {
+    vertical-align: bottom;
+  }
 </style>
